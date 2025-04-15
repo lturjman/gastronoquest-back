e89f5d527aaa125c465058dc4489580608dba131
Laura Turjman <contact@lauraturjman.fr>
2025-04-15 15:37:38 +0200

    route index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,38 @@ const express = require("express");
 const router = express.Router();
 require("../models/connection");
 
-let challenges = require("../models/challenges");
+// CHALLENGES
+const Challenge = require("../models/challenges");
+
+// Route GET pour récupérer tous les challenges
+router.get("/challenges", async (req, res) => {
+  try {
+    const challenges = await Challenge.find();
+    res.json({ result: true, challenges });
+  } catch (error) {
+    res.status(500).json({
+      result: false,
+      message: "Erreur lors de la récupération des challenges",
+      error,
+    });
+  }
+});
+
+// QUIZ
+const Quiz = require("../models/quiz");
+
+// Route GET pour récupérer tous les quiz
+router.get("/quiz", async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+    res.json({ result: true, quizzes });
+  } catch (error) {
+    res.status(500).json({
+      result: false,
+      message: "Erreur lors de la récupération des quiz",
+      error,
+    });
+  }
+});
 
 module.exports = router;
