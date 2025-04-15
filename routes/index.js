fecf9506e93fc52f09e3b898bb75646c2266604d
Laura Turjman <contact@lauraturjman.fr>
2025-04-15 14:15:24 +0200

    json

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+require("../models/connection");
+
+let challenges = require("../models/challenges");
 
 module.exports = router;
