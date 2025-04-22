9f5b9cdb2678d3c64d2a95f8bda8250f2be25a78
Morgan Dumond <morgan.dumond@outlook.com>
2025-04-22 11:48:14 +0200

    Placement des imports au début du document et sélection des informations renvoyées par GET /quiz modifiée en /quizzes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
-require("../models/connection");
-const { validateFields } = require("../middlewares/validateFields");
-
-// CHALLENGES
 const Challenge = require("../models/challenges");
+const Quiz = require("../models/quizzes");
+const { validateFields } = require("../middlewares/validateFields");
 
 // Route GET pour récupérer tous les challenges
 router.get("/challenges", async (req, res) => {
   try {
-    const challenges = await Challenge.find();
+    const challenges = await Challenge.find().select("-__v");
     res.json({ result: true, challenges });
   } catch (error) {
     res.status(500).json({
@@ -20,13 +18,10 @@ router.get("/challenges", async (req, res) => {
   }
 });
 
-// QUIZ
-const Quiz = require("../models/quizzes");
-
-// Route GET pour récupérer tous les quiz
-router.get("/quiz", async (req, res) => {
+// Route GET pour récupérer tous les quizz
+router.get("/quizzes", async (req, res) => {
   try {
-    const quizzes = await Quiz.find();
+    const quizzes = await Quiz.find().select("-questions -__v");
     res.json({ result: true, quizzes });
   } catch (error) {
     res.status(500).json({
@@ -44,7 +39,7 @@ router.get(
   async (req, res) => {
     try {
       const { quizId } = req.params;
-      const data = await Quiz.findById(quizId);
+      const data = await Quiz.findById(quizId).select("-__v");
       res.json({ result: true, data });
     } catch (error) {
       res.status(500).json({
