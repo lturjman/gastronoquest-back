78decdbb3fd81d17986d81ae31391c496b90eb34
Nathan-Mrlt <nathan.mrlt@gmail.com>
2025-04-23 11:59:34 +0200

    add comments on middleware

--- middlewares/validateFields.js
+++ middlewares/validateFields.js
@@ -2,11 +2,15 @@
 
 const validateFields = (requiredFields, location = "body") => {
   return (req, res, next) => {
+    // Récupération des éléments à vérifier dans la requête
     const data = req[location];
+    // On filtre pour ne garder que les éléments manquants
+    // Utilisation de undefined pour éviter de considérer comme manquants des champs avec une valeur falsy
     const missingFields = requiredFields.filter(
       (field) => data[field] === undefined
     );
 
+    // On envoie une erreur en cas de champ manquant
     if (missingFields.length > 0) {
       return res.status(400).json({
         result: false,
